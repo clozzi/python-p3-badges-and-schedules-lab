179384989e6a37cd51c468ce9712e0aaf8b72de6
Chris Lozzi <clozzi12@gmail.com>
2023-12-15 12:17:54 -0500

    tests passing

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,28 @@
 def badge_maker(name):
-    return None
+    return f"Hello, my name is {name}."
 
 def batch_badge_creator(names):
-    return None
+    speaker_list = []
+    for name in names:
+        message = f"Hello, my name is {name}."
+        speaker_list.append(message)
+    return speaker_list
 
 def assign_rooms(names):
-    return None
+    room_assignments = []
+    room = 1
+    for name in names:
+        message = f"Hello, {name}! You'll be assigned to room {room}!"
+        room_assignments.append(message)
+        room += 1
+    return room_assignments
 
 def printer(names):
-    return None
+    badges = batch_badge_creator(names)
+    rooms = assign_rooms(names)
+
+    for badge in badges:
+        print(badge)
+
+    for room in rooms:
+        print(room)
